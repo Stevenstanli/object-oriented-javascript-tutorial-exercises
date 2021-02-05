30860dccf66317154e6a2de922ea0907808c1938
Steven <stanli-peter19@hotmail.com>
2021-02-05 18:10:35 +0000

    object oriented javascript tutorial exercises steven espinoza

--- exercises/09-Calculated-property/app.js
+++ exercises/09-Calculated-property/app.js
@@ -1,16 +1,25 @@
-class Person{
-	//the constructor receives 3 params
-	constructor(year, month, date){
-		this._birthday = new Date(year, month, date);
-	}
-	
-	getAge() { // birthday is a date
-	    let ageDifMs = Date.now() - this._birthday.getTime();
-	    let ageDate = new Date(ageDifMs); // milliseconds from epoch
-	    return Math.abs(ageDate.getUTCFullYear() - 1970);
-	}
-	
-	
+class Person {
+    //the constructor receives 3 params
+    constructor(year, month, date) {
+        this._birthday = new Date(year, month, date);
+    }
+
+    getAge() { // birthday is a date
+        let ageDifMs = Date.now() - this._birthday.getTime();
+        let ageDate = new Date(ageDifMs); // milliseconds from epoch
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
+
+    canDrinkAlcohol() {
+        //your code here
+        if (this.getAge() >= 21) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
 }
 
 let mario = new Person(1985, 10, 20);
